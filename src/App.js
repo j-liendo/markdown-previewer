539852a3eb2054dcce3540ce2994e757e0ef8684
josh1502-bit <72353245+josh1502-bit@users.noreply.github.com>
2021-11-07 17:41:16 -0400

    -

--- src/App.js
+++ src/App.js
@@ -1,50 +1,116 @@
-import "./App.css";
 import React from "react";
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import './App.css'
 
 class App extends React.Component {
-  constructor(props) {
+  constructor() {
     super();
     this.state = {
-      text: '',
-      chat: ['Hola']
+      input: placeHolder,
+      theme: 'light'
     }
-    this.controllerWriter = this.controllerWriter.bind(this);
-    this.addChat = this.addChat.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.switchTheme = this.switchTheme.bind(this);
   }
 
-  controllerWriter (e) {
+  handleInput(e) {
+    console.log(e.target.value)
     this.setState({
-      text: e.target.value
+      input: e.target.value
     })
   }
 
-  addChat() {
-    
-    const line = this.state.text;
-    this.setState((state) => ({
-      chat: [...state.chat, line],
-      text: ''
-    }))
-    console.log(this.state.chat)
+  switchTheme () {
+    if (this.state.theme === 'light') {
+      this.setState({
+        theme: 'dark'
+      })
+    } else {
+      this.setState({
+        theme: 'light'
+      })
+
+    }
   }
 
   render() {
-    const chatList = this.state.chat.map(line => <h3>{line}</h3>);
+    // const output = this.state.input.map(line => <MarkdownPreview source={line} />);
+    const markdown = this.state.input
     return (
-      <div>
-        {chatList}
-        <input 
-          type="text" 
-          name="name" 
-          id="name" 
-          onChange={this.controllerWriter}
-          placeholder="Write..."
-          value={this.state.text}
-        />
-        <button type="submit" onClick={this.addChat} id="btn">send</button>
+      <div className={this.state.theme}>
+        <div className="flex">
+          <h1>Markdown previewer</h1>
+        <div className="main">
+          <textarea
+            name="textArea"
+            onChange={this.handleInput} 
+            id="input"
+            value={this.state.input}
+            placeholder="Markdown code..."></textarea>
+          <div id="output">
+            <ReactMarkdown
+              children={markdown} 
+              remarkPlugins={[remarkGfm]}
+            />
+          </div>
+        </div>
+        </div>
+        <button className="themeBtn" onClick={this.switchTheme}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-brightness-high" viewBox="0 0 16 16">
+          <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
+        </svg>
+        </button>
+        <footer>
+          Created with React by <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+</svg> <a href="https://github.com/josh7bit/markdown-previewer">Josh7bit</a> 
+        </footer>
       </div>
     );
   }
 }
+
+const placeHolder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+`;
 
 export default App;
